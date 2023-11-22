3b11ac6c5cdfa109184cd2819890d216a494cb48
LuisGustavo377 <luuis.gustavo.andrade7@gmail.com>
2023-11-21 23:50:48 -0300

    feature: Config Tables

---
+++ database/migrations/2023_11_22_015817_create_musicos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('musicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->date('dataNascimeto');
+            $table->string('endereco');
+            $table->string('email');
+            $table->date('dataRegistro');
+            $table->string('frequencia');
+            $table->timestamps();
+
+            // $table->foreign('emprestimo_id')->references('id')->on('emprestimos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('musicos');
+    }
+};
